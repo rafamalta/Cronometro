164d402f9a95d83946e6f9b42a8bed362e8d7781
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-16 14:37:51 -0300

    Estrutura

---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cronometro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
+    }
+}
