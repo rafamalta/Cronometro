dcb12710afe5606c06ab163718c04a4b05d29817
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-17 11:16:09 -0300

    Calculando o tempo

--- Cronometro/Cronometro/Program.cs
+++ Cronometro/Cronometro/Program.cs
@@ -13,33 +13,44 @@
             Console.WriteLine("S - Segundo");
             Console.WriteLine("M - Minuto");
             Console.WriteLine("0 - Sair");
-            Console.WriteLine("Quanto tempo deseja contar?");
+            Console.Write("Quanto tempo deseja contar? ");
 
             string resposta = Console.ReadLine().ToLower();            
             char tipo = char.Parse(resposta.Substring(resposta.Length - 1, 1));
-            int tempo = int.Parse(resposta.Substring(0, resposta.Length - 1));
-            
-            Console.WriteLine(tipo);
-            Console.WriteLine(tempo);
+            int tempoTotal = int.Parse(resposta.Substring(0, resposta.Length - 1));
+            int mulplicador = 1;
+
+            if (tipo == 'm')
+            {
+                mulplicador = 60;
+            }
+
+            if (tempoTotal == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Start(tempoTotal * mulplicador);
 
             Console.ReadKey();
         }
 
-        static void Start(int time)
+        static void Start(int tempoTotal)
         {
-            int currentTime = 0;
+            int tempoAtual = 0;
 
-            while (currentTime != time)
+            while (tempoAtual != tempoTotal)
             {
                 Console.Clear();
-                currentTime++;
-                Console.WriteLine(currentTime);
+                tempoAtual++;
+                Console.WriteLine(tempoAtual);
                 Thread.Sleep(1000);
             }
 
             Console.Clear();
-            Console.WriteLine("Stopwatch finalizado!");
+            Console.WriteLine("Cronômetro finalizado!");
             Thread.Sleep(2500);
+            Menu();
         }        
     }
 }
